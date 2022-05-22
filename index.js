2e471e0f19ccdd9adcba5a278ad6c3169edbd9c2
Ahmed12ibad <ahmedhamid777fast@gmail.com>
2022-05-22 12:50:37 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,106 @@
+var name = '';
+name= 'ahmed';
+//alert=(name);
+
+//var num1= 10;
+//var num2= "10";
+//console.log(num1+num2);
+
+var num1= 10;
+var num2= 5;
+var num3= 30;
+var num4= 30;
+
+alert ((num1+num2),num3+num4);
+
+
+//var  a= 2
+//var b= 1
+ 
+//var result= --a -  --b + ++b + b++;
+//alert (result)
+ 
+//document.write( "<h1>" +name+ "<h1>"  )
+
+//var star ="ghous"// string
+//var num =65// number
+ //var bool = true // bolion
+//!!"ghous"
+//true
+//!!false
+//false
+//!!null
+//false
+//!!undefined
+//false
+//!!nahi
+//false
+//!!0
+//false
+//!!
+//false
+//alert=((1+2) 4*1);
+
+
+//var num 1 = +prompt("Enter yoiur num 1")
+//var num 2 = +prompt("Enter yoiur num 2")
+//var= result
+//consol . log(num 1 + num 2)
+//2===2  true
+//2==2 false
+
+
+//var num1 = +prompt("enter your number")
+//var num2 = +prompt('enter your number')
+//var opt  =  prompt("enter your opt (+ / - +)"   )
+//var result;
+
+//if(opt==="+") {
+  //  result= num1+num2
+//}
+//if(opt==="-") {
+  //  result= num1-num2
+//}
+//if(opt==="*") {
+  //  result= num1*num2
+//}
+//if(opt==="/") {
+  //  result= num1/num2
+//}
+//console.log(result)
+
+
+
+
+
+
+//.next class
+
+//var star = "ahmed /n shayan"
+//alert ( star)
+
+10 <20
+true
+30<20
+false
+10>9
+true
+10>10
+false
+10>=10 
+true
+10 <=10
+true
+
+
+
+var exp = +prompt("enter your age")
+
+if(exp > 2){
+alert ("not"  )
+}
+
+
+if(exp < 2){
+    alert ("not"  )
+    }s
